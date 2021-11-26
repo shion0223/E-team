d2345c81660600d5a35f37586a3ae82088c1cab8
shion <m75fb2ded7qwf@gmail.com>
2021-11-26 12:22:00 +0900

    APIの削除

--- mushi-map-world/src/app/app.module.ts
+++ mushi-map-world/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GoogleMapsModule } from '@angular/google-maps';
 
 @NgModule({
   declarations: [
@@ -12,7 +11,6 @@ import { GoogleMapsModule } from '@angular/google-maps';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    GoogleMapsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
