3076c3aa7fc2db9be58022548441be1e24532617
shion <m75fb2ded7qwf@gmail.com>
2021-12-07 14:52:33 +0900

    新しいバージョンに変更

--- mushi-map-world/karma.conf.js
+++ mushi-map-world/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/mushi-map-world'),
+      dir: require('path').join(__dirname, './coverage/Mushi-Map-World'),
       subdir: '.',
       reporters: [
         { type: 'html' },
