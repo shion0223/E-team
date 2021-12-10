33b008900b8b19039e1e93774ffcf2a6d2d61556
DESKTOP-GE3AVCO\tetsu <zeal20310013@fsgcl.jp>
2021-12-10 10:10:16 +0900

    ユーザー登録画面のコンポーネントの作成

--- mushi-map-world/src/app/app.module.ts
+++ mushi-map-world/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { DetailComponent } from './pages/detail/detail.component';
 import { SpotComponent } from './pages/spot/spot.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 
 @NgModule({
@@ -16,6 +17,7 @@ import { LoginComponent } from './pages/login/login.component';
     SpotComponent,
     HomeComponent,
     LoginComponent,
+    RegisterComponent,
 
 
   ],
