d3ee77f3864f5d4eec69ade88ab9f9e82633eb3e
shion <m75fb2ded7qwf@gmail.com>
2022-01-14 09:50:09 +0900

    ルーターを設定して、画面遷移できるようにした

--- mushi-map-world/src/app/app-routing.module.ts
+++ mushi-map-world/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CompleteComponent } from './pages/complete/complete.component';
+import { DetailComponent } from './pages/detail/detail.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { SpotComponent } from './pages/spot/spot.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+
+  {path:"", component:HomeComponent},
+  {path:"complete", component:CompleteComponent},
+  {path:"detail", component:DetailComponent},
+  {path:"login", component:LoginComponent},
+  {path:"register", component:RegisterComponent},
+  {path:"spot", component:SpotComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
